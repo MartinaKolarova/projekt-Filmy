0b0ce4c8f83b4ddbf500a0df6377147e184d8b4e
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-05-31 18:47:37 +0200

    Solution of homework

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 /* tady bude tvůj kód */
-console.log('JavaScript ve stránce funguje');
+const movieList = document.querySelector('#movies');
+
+const Movie = ({ posterUrl, title, url, year, genres }) => {
+  return `
+  <div class="movie">
+        <img class="movie__img" src="${posterUrl}"alt=${title}>
+        <h2 class="movie__title"><a href="${url}">${title}</a></h2>
+        <p class="movie__year">${year}</p>
+        <p class="movie__genre">${genres.join(', ')}</p>
+      </div>
+      `;
+};
+
+const showMovies = (movies) => {
+  movies.sort((a, b) => a.title.localeCompare(b.title));
+  movies.forEach((movie) => {
+    movieList.innerHTML += Movie(movie);
+  });
+};
+
+const sortMoviesUp = () => {
+  movies.sort((a, b) => a.year - b.year);
+};
+
+const sortMoviesDown = () => {
+  movies.sort((a, b) => b.year - a.year);
+};
+
+const sortUpButton = document.querySelector('.button__sort--up');
+sortUpButton.addEventListener('click', sortMoviesUp);
+
+const sortDownButton = document.querySelector('.button__sort--down');
+sortUpButton.addEventListener('click', sortMoviesDown);
+
+const fetchMovie = async () => {
+  const response = await fetch(
+    'https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies',
+  );
+  const data = await response.json();
+  return data;
+};
+
+const main = async () => {
+  const movies = await fetchMovie();
+  showMovies(movies);
+};
+main();
