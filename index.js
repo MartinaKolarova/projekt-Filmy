d27c322b485feeffc45e1c2d0e8f7bce8202daa9
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-05-31 22:05:37 +0200

    Some ganges but not working

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const movieList = document.querySelector('#movies');
+const movies = [];
 
 const Movie = ({ posterUrl, title, url, year, genres }) => {
   return `
   <div class="movie">
-        <img class="movie__img" src="${posterUrl}"alt=${title}>
+        <img class="movie__img" src="${posterUrl}" alt="${title}">
         <h2 class="movie__title"><a href="${url}">${title}</a></h2>
         <p class="movie__year">${year}</p>
         <p class="movie__genre">${genres.join(', ')}</p>
@@ -11,7 +12,17 @@ const Movie = ({ posterUrl, title, url, year, genres }) => {
       `;
 };
 
-const showMovies = (movies) => {
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((movie) => {
+      movies.push(movie);
+    });
+    showMovies();
+  });
+
+const showMovies = () => {
+  movieList.innerHTML = '';
   movies.sort((a, b) => a.title.localeCompare(b.title));
   movies.forEach((movie) => {
     movieList.innerHTML += Movie(movie);
@@ -20,12 +31,12 @@ const showMovies = (movies) => {
 
 const sortMoviesUp = () => {
   movies.sort((a, b) => a.year - b.year);
-  showMovies(movies);
+  showMovies();
 };
 
 const sortMoviesDown = () => {
   movies.sort((a, b) => b.year - a.year);
-  showMovies(movies);
+  showMovies();
 };
 
 const sortUpButton = document.querySelector('.button__sort--up');
@@ -33,7 +44,3 @@ sortUpButton.addEventListener('click', sortMoviesUp);
 
 const sortDownButton = document.querySelector('.button__sort--down');
 sortDownButton.addEventListener('click', sortMoviesDown);
-
-fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
-  .then((response) => response.json())
-  .then(showMovies);
